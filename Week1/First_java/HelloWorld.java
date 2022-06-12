644321346512a8b94e9d0e80b8da0446d0c182df
nicholasgoodwin3 <nicholasgoodwin3@gmail.com>
2022-06-12 10:21:37 -0400

    first

---
+++ Week1/First_java/HelloWorld.java
@@ -0,0 +1,9 @@
+package Week1.First_java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My name is Nicholas Goodwin");
+        System.out.println("I am 29 years old");
+        System.out.println("My hometown is Swansea, SC");
+    }
+}
